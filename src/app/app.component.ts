c4922472bbc24ab7d4cb06b3a5a48450a850bcbf
Feng Chang <fengchang1.618@gmail.com>
2017-07-02 17:51:54 +0800

    try to push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { MdAutocompleteModule } from '@angular/material';
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/startWith';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,66 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  stateCtrl: FormControl;
+  filteredStates: any;
+
+  states = [
+    'Alabama',
+    'Alaska',
+    'Arizona',
+  ];
+
+  constructor() {
+    this.stateCtrl = new FormControl();
+    this.filteredStates = this.stateCtrl.valueChanges
+        .startWith(null)
+        .map(name => this.filterStates(name));
+  }
+
+  filterStates(val: string) {
+    return val ? this.states.filter(s => s.toLowerCase().indexOf(val.toLowerCase()) === 0)
+               : this.states;
+  }
+
+  favoriteSeason: string;
+
+  seasons = [
+    'Winter',
+    'Spring',
+    'Summer',
+    'Autumn',
+  ];
+
+  foods = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+
+
+  autoTicks = false;
+  disabled = false;
+  invert = false;
+  max = 100;
+  min = 0;
+  showTicks = false;
+  step = 1;
+  thumbLabel = false;
+  value = 0;
+  vertical = false;
+
+  get tickInterval(): number | 'auto' {
+    return this.showTicks ? (this.autoTicks ? 'auto' : this._tickInterval) : null;
+  }
+  set tickInterval(v) {
+    this._tickInterval = Number(v);
+  }
+  private _tickInterval = 1;
+
+  color = 'accent';
+  checked = false;
+
+   ngOnInit() {
+
+   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+import { ReactiveFormsModule  } from '@angular/forms';
+import { MdAutocompleteModule, MdInputModule } from '@angular/material';
+
+import { MdCheckboxModule } from '@angular/material';
+
+import { MdDatepickerModule, MdNativeDateModule } from '@angular/material';
+
+import { MdRadioModule } from '@angular/material';
+
+import { FormsModule } from '@angular/forms';
+
+import { MdSelectModule } from '@angular/material';
+
+import { MdSliderModule } from '@angular/material';
+
+import { MdCardModule } from '@angular/material';
+
+import { MdSlideToggleModule } from '@angular/material';
+
 import { AppComponent } from './app.component';
+import 'hammerjs'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MdAutocompleteModule,
+    MdInputModule,
+    MdCheckboxModule,
+    MdDatepickerModule,
+    MdNativeDateModule,
+    MdRadioModule,
+    FormsModule,
+    MdSelectModule,
+    MdSliderModule,
+    MdCardModule,
+    MdSlideToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
